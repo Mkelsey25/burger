b12c569be1920436dc32a8b533e9f7ba1ca15e4a
Mkelsey <mkh13b@my.fsu.edu>
2018-08-20 12:19:31 -0400

    changed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser"); 
 var mySql = require("mysql");
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 49066;
 var app = express();
 
 
