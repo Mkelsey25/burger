047e6368064f5207ae487b8c5ed44e9d62aef853
Mkelsey <mkh13b@my.fsu.edu>
2018-08-20 15:06:20 -0400

    changed connection.js to help with heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,19 @@
-var mysql = require("mysql");
+var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection(
-  {
-  host: "localhost",
-  PORT: 3306,
-  user: "root",
-  password: "Wubbs#2557",
-  database: "burgers_db"
-});
+// For Heroku Deployment vs. Local MySQL Database
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Wubbs#2557',
+    database : 'burgers_db'
+  });
+}
 
-// Make connection.
-/*connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});*/
 
-// Export connection for our ORM to use.
+// Export the Connection
 module.exports = connection;
